ed2cea3d3cf268176f46190cd58dc1f6b3e0336f
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-09 22:08:25 -0600

    commit example

--- Mileage/Program.cs
+++ Mileage/Program.cs
@@ -9,13 +9,14 @@ namespace Mileage
             Console.Write("How many miles have you driven? ");
 
             // Get input from the user and save it to a variable.
+            float miles = float.Parse(Console.ReadLine());
 
             Console.Write("How many gallons of gas have you consumed? ");
-
             // Get input from the user and save it to a second variable.
+            float gallons = float.Parse(Console.ReadLine());
 
             // Output - Your miles per gallon is (computed value)
-
+            Console.WriteLine("Your miles per gallon is " + miles / gallons);
             Console.ReadLine();
         }
     }
